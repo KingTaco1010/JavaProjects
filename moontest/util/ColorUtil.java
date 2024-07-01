88c06cd57343890967789dcedeca4d18eae69e61
KingTaco1010 <123maestro123king@gmail.com>
2024-07-01 16:49:52 +0500

    Delete moontest directory

--- moontest/util/ColorUtil.java
+++
@@ -1,9 +0,0 @@
-package org.moonstudio.moontest.util;
-
-import org.bukkit.ChatColor;
-
-public class ColorUtil {
-    public static String colorAlternate(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
-}
